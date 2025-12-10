a89807ffd81acaeac3805a4f75215959329d6e3b
dgny01 <yildizdoganay2003@gmail.com>
2025-12-10 13:56:24 +0300

    Fix linting errors and update flake8 config

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,8 +5,8 @@ Calculator Funciton
 
 def add(x, y):
     """"Add x and y and returnresult."""
-    return x+y
+    return x + y
 
-def subtract(x,y):
+def subtract(x, y):
     """"Subtract x from y and return result."""
-    return y - x
+    return y - x 
