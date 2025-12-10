4205c96f0356dd7df36dd94ef18593d46555e04d
dgny01 <yildizdoganay2003@gmail.com>
2025-12-10 14:24:36 +0300

    Fix typo and linting errors in wait_for_db

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,11 +1,13 @@
 """
-Calculator Funciton
+Calculator functions.
 """
 
+
 def add(x, y):
-    """"Add x and y and returnresult."""
+    """Add x and y and return result."""
     return x + y
 
+
 def subtract(x, y):
-    """"Subtract x from y and return result."""
-    return y - x
+    """Subtract x from y and return result."""
+    return x - y
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,5 +1,5 @@
 """
-Django command to wait for the database to be available
+Django command to wait for the database to be available.
 """
 import time
 
@@ -21,8 +21,8 @@ class Command(BaseCommand):
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2Error, OperationalError):
-                self.stdout.write('Database unavaliable, waiting 1 second...')
+                self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-            
-        self.stdout.write(self.style.SUCCESS('Database available'))
+
+        self.stdout.write(self.style.SUCCESS('Database available!'))
         
